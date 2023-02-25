8c6b346048ba973e89a675c746ddc7eb8357efee
DimonD <dprohorchuk2002@gmail.com>
2023-02-25 13:48:03 +0200

    Extract stage done

---
+++ TransformTransactions/Extractors/FileDataExtractor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransformTransactions.Extractors
+{
+    internal class FileDataExtractor : IDataExtractor
+    {
+        private string? _filePath;
+        public string? FilePath
+        {
+            get => _filePath;
+            set
+            {
+                if (!File.Exists(value))
+                {
+                    throw new FileNotFoundException($"The file {value} could not be found.");
+                }
+                _filePath = value;
+            }
+        }
+
+        public List<string> ExtractData()
+        {
+            if (string.IsNullOrEmpty(_filePath))
+            {
+                throw new InvalidOperationException("The FilePath property must be set before calling ExtractData().");
+            }
+
+            List<string> data = new();
+
+            using var reader = new StreamReader(_filePath);
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                data.Add(line);
+            }
+
+            return data;
+        }
+    }
+}
---
+++ TransformTransactions/Extractors/IDataExtractor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransformTransactions.Extractors
+{
+    internal interface IDataExtractor
+    {
+        public List<string> ExtractData();
+    }
+}
